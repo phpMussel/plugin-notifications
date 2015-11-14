0792ee55a8656f1e0888c16652aab6b247217278
Caleb Mazalevskis <maikuolan@gmail.com>
2015-11-15 03:48:08 +0800

    Initial commit.
    In theory, this should work correctly and should be ready for general
    use; However, testing is still required (I haven't yet done any live
    testing).

---
+++ README.md
@@ -0,0 +1,27 @@
+## **What is phpMussel?**
+
+An ideal solution for shared hosting environments, where it's often not possible to utilise or install conventional anti-virus protection solutions, phpMussel is a PHP script designed to **detect trojans, viruses, malware and other threats** within files uploaded to your system wherever the script is hooked, based on the signatures of [ClamAV](http://www.clamav.net/) and others.
+
+## **What's this repository for?**
+
+This repository, "phpMussel-plugin-notifications", is the repository for a phpMussel plugin that allows you to receive email notifications from phpMussel whenever a file upload is blocked (requires PHP "mail" function to be correctly configured).
+
+The main repository: [phpMussel](https://github.com/Maikuolan/phpMussel).
+
+This repository: [phpMussel-plugin-notifications](https://github.com/Maikuolan/phpMussel-plugin-notifications).
+
+## **How to install?**
+
+1. Add the following section to your `phpmussel.ini` file and edit accordingly:
+
+```
+[notifications]
+; "to_addr" is the email address for sending notifications to.
+to_addr='username@domain.tld'
+; "from_addr" is the email address cited as the origin for all sent notifications.
+from_addr='username@domain.tld'
+```
+
+2. Upload the "notifications" directory of this repository and all its contents to the "plugins" directory of your phpMussel installation (the "plugins" directory is a sub-directory of the "vault" directory).
+
+That's everything! :-)
---
+++ notifications/plugin.php
@@ -0,0 +1,44 @@
+<?php
+/*    _____  _     _  _____  _______ _     _ _______ _______ _______
+ <   |_____] |_____| |_____] |  |  | |     | |______ |______ |______ |        >
+     |       |     | |       |  |  | |_____| ______| ______| |______ |_____
+
+ This "plugin.php" file belongs to a plugin of phpMussel.
+
+ PLUGIN INFORMATION BEGIN
+        Plugin Name: Notifications.
+      Plugin Author: Caleb M / Maikuolan.
+     Plugin Version: 1.0.0
+   Download Address: https://github.com/Maikuolan/phpMussel-plugin-notifications
+    Min. Compatible: 0.9.0
+    Max. Compatible: -
+       Tested up to: 0.9.0
+      Last Modified: 2015.11.15
+ Plugin Description: Receive email notifications from phpMussel whenever a file
+                     upload is blocked (requires PHP "mail" function to be
+					 correctly configured; for more information, please refer
+					 to http://php.net/manual/en/function.mail.php).
+ PLUGIN INFORMATION END */
+
+if(!defined('phpMussel'))die('[phpMussel] This should not be accessed directly.');
+
+phpMussel_Register_Hook('phpMussel_Notify','before_html_out');
+
+if(!isset($MusselConfig['notifications']))$MusselConfig['notifications']=array();
+if(!isset($MusselConfig['notifications']['to_addr']))$MusselConfig['notifications']['to_addr']='';
+if(!isset($MusselConfig['notifications']['from_addr']))$MusselConfig['notifications']['from_addr']='';
+
+function phpMussel_Notify($input)
+	{
+	if(!$GLOBALS['MusselConfig']['notifications']['to_addr']||!$GLOBALS['MusselConfig']['notifications']['from_addr']||empty($GLOBALS['whyflagged']))return false;
+	$NotifyLang=(!file_exists($GLOBALS['vault'].'plugins/notifications/template.'.$GLOBALS['MusselConfig']['general']['lang'].'.eml'))?'en':$GLOBALS['MusselConfig']['general']['lang'];
+	$NotifyVars=array();
+	$NotifyVars['whyflagged']=$GLOBALS['whyflagged'];
+	$NotifyVars['ipaddr']=$_SERVER[$GLOBALS['MusselConfig']['general']['ipaddr']];
+	$NotifyVars['time']=date('r');
+	$content=phpMusselV($NotifyVars,phpMusselFile($GLOBALS['vault'].'plugins/notifications/template.'.$NotifyLang.'.eml',0,true));
+	mail($GLOBALS['MusselConfig']['notifications']['to_addr'],$GLOBALS['MusselConfig']['lang']['denied'],$content,"MIME-Version: 1.0\nContent-type: text/plain; charset=iso-8859-1\nFrom: ".$GLOBALS['MusselConfig']['notifications']['from_addr'],'-f'.$GLOBALS['MusselConfig']['notifications']['from_addr']);
+	return true;
+	}
+
+?>
